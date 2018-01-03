86c6b8e88df8f6f9faf3ed63cd901a7fba1bed70
Josh Powell <Josh.Powell@softwire.com>
2018-01-03 14:26:21 +0000

    Now uses arrays for easier reversal

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-for(var i = 0; i <= 100; i++){
-    var string = "";
+for (var i = 1; i <= 256; i++) {
+    var arr = [];
     if (i % 3 === 0){
-        string = string.concat("Fizz");
+        arr.push("Fizz");
+    }
+    if (i % 13 === 0){
+        arr.push("Fezz");
     }
     if (i % 5 === 0){
-        string = string.concat("Buzz");
+        arr.push("Buzz");
     }
-    if (string == ""){
-        string = i;
+    if (i % 7 === 0){
+        arr.push("Bang");
     }
-    console.log(string);
+    if (i % 11 === 0){
+        if (i % 13 === 0){
+            arr = ["Fezz"];
+        }
+        else{
+            arr.length = 0;
+        }
+        arr.push("Bong");
+    }
+if (i % 17 == 0)
+{
+    arr = arr.reverse();
+}
+    if (arr.length === 0){
+        arr.push(i);
+    }
+    string = "";
+    for(var j=0;j<arr.length;j++)
+    {
+       string = string.concat(arr[j]);
+    }
+console.log(string);
 }
