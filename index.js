10a39bce37e322fc45ba26c55babf1f4f3fc9ec2
Josh Powell <Josh.Powell@softwire.com>
2018-01-03 15:45:41 +0000

    Will now show optionally entered rules when they are done.

--- index.js
+++ index.js
@@ -2,59 +2,58 @@ const readline = require('readline-sync');
 console.log("Please enter the highest value you wish to count to");
 var response = readline.prompt();
 response = parseInt(response);
-var done = false;
-while (!done){
+var rules ={};
+done = false
+while (!done) {
     console.log("Please enter the rules you wish to apply, one by one, pressing enter after each one.");
     console.log("When you are done just press enter on a blank line.");
     console.log("Possible rules: 3, 5, 7, 11, 13, 17");
-    var rule = readline.prompt();
-    if (rule == ""){
-        done = true;
-    }
-    if (rule == "T" || rule == "t"){
-     var test = true;
-    }
-    if (rule == "3")
-    {
-        var Fizz = true;
-    }
-    if (rule == "13")
-    {
-        var Fezz = true;
-    }
-    if (rule == "5")
-    {
-        var Buzz = true;
-    }
-    if (rule == "7")
-    {
-        var Bang = true;
-    }
-    if (rule == "11")
-    {
-        var Bong = true;
-    }
-    if (rule == "1")
-    {
-        var Reverse = true;
+    console.log("Current rules:");
+    for (var item in rules) {
+        if (rules[item] = true) {
+            console.log(item);
+        }
     }
 
+    var rule = readline.prompt();
+    if (rule == "") {
+        done = true;
+    }
+    if (rule == "3") {
+        rules.Fizz = true;
+    }
+    if (rule == "13") {
+        rules.Fezz = true;
+    }
+    if (rule == "5") {
+        rules.Buzz = true;
+    }
+    if (rule == "7") {
+        rules.Bang = true;
+    }
+    if (rule == "11") {
+        rules.Bong = true;
+    }
+    if (rule == "1") {
+        rules.Reverse = true;
+    }
+}
 
 for (var i = 1; i <= response; i++) {
     var arr = [];
-    if (i % 3 === 0 && Fizz){
+    if (i % 3 === 0 && rules.Fizz){
         arr.push("Fizz");
     }
-    if (i % 13 === 0 && Fezz){
+    if (i % 13 === 0 && rules.Fezz){
         arr.push("Fezz");
     }
-    if (i % 5 === 0 && Buzz){
+    if (i % 5 === 0 && rules.Buzz){
         arr.push("Buzz");
     }
-    if (i % 7 === 0 && Bang){
+    if (i % 7 === 0 && rules.Bang){
         arr.push("Bang");
     }
-    if (i % 11 === 0 && Bong){
+    if (i % 11 === 0 && rules.Bong){
         if (i % 13 === 0){
             arr = ["Fezz"];
         }
@@ -63,7 +62,7 @@ for (var i = 1; i <= response; i++) {
         }
         arr.push("Bong");
     }
-    if (i % 17 == 0 && Reverse){
+    if (i % 17 == 0 && rules.Reverse){
     arr = arr.reverse();
     }
     if (arr.length === 0){
